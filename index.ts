142bb071827995ab008872fd7ff35359bd47f248
projectn8n2026 <iojvanpopadic@gmail.com>
2026-02-07 22:11:35 +0100

    Create index.ts
    Descrizione: Questo è il "centralino" della cartella componenti. Non contiene grafica, ma serve a esporre all'esterno i pezzi pregiati che abbiamo archiviato. Grazie a questo file, puoi importare tutto con una sola riga di codice, mantenendo il progetto pulito e professional

---
+++ index.ts
@@ -0,0 +1,5 @@
+// Integrated components from external sources
+// Adapted to Nexus Logica design system
+
+export { default as DigitalTwinWidget } from "./DigitalTwinWidget";
+export { default as TimelineWidget } from "./TimelineWidget";
